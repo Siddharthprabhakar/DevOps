a0bd552e5da33490d4dcd1ece6dcd78381e953f5
Siddharth Prabhakar <sid130803@gmail.com>
2024-10-17 23:20:53 +0530

    terraform files

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+# outputs.tf
+
+output "instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.weduflex-app.public_ip
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.eduflex-app.id
+}
+
+output "instance_arn" {
+  description = "The ARN of the EC2 instance"
+  value       = aws_instance.eduflex-app.arn
+}
