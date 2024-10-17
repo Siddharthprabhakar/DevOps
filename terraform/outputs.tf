57529b6581eade643d575b2b6ce47536c3495254
VK <vishanto95@gmail.com>
2024-10-17 17:22:18 +0530

    Changed instance type

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_public_ip" {
   description = "The public IP of the EC2 instance"
-  value       = aws_instance.worksync_app.public_ip
+  value       = aws_instance.eduflex-app.public_ip
 }
 
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.worksync_app.id
+  value       = aws_instance.eduflex-app.id
 }
 
 output "instance_arn" {
   description = "The ARN of the EC2 instance"
-  value       = aws_instance.worksync_app.arn 
+  value       = aws_instance.eduflex-app.arn 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@
 
 variable "instance_type" {
   description = "The type of instance to deploy"
-  default     = "t2.medium"  # Change the instance type if needed
+  default     = "t4g.small"  # Change the instance type if needed
 }
 
 variable "key_name" {
