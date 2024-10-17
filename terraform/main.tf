82bc9c88fc14f84125ca8079b8c09f418525d108
Siddharth Prabhakar <sid130803@gmail.com>
2024-10-18 00:38:17 +0530

    key name issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@
 resource "aws_instance" "eduflex-app" {
   ami           = "ami-0a2363a9cff180a64"  # Amazon Linux 2 in ap-southeast-2
   instance_type = "t2.micro"
-  key_name = "your-ec2-key-name"
-
+  key_name      = var.key_name             # Variable for the SSH key
+  
   user_data = file("userdata.sh") 
 
   tags = {
