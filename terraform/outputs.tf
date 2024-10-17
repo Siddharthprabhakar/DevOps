613a26e339b289176cebd0256a74b32a0efe316d
VK <vishanto95@gmail.com>
2024-10-17 17:07:49 +0530

    Update resource names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,14 @@
-# outputs.tf
-
 output "instance_public_ip" {
   description = "The public IP of the EC2 instance"
-  value       = aws_instance.weduflex-app.public_ip
+  value       = aws_instance.worksync_app.public_ip
 }
 
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.eduflex-app.id
+  value       = aws_instance.worksync_app.id
 }
 
 output "instance_arn" {
   description = "The ARN of the EC2 instance"
-  value       = aws_instance.eduflex-app.arn
+  value       = aws_instance.worksync_app.arn 
 }
