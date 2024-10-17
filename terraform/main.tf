71e56232ef277a0eed666c7beb1e180763e400bb
Siddharth Prabhakar <sid130803@gmail.com>
2024-10-18 00:32:36 +0530

    minor changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # main.tf
 
-resource "aws_instance" "eduflex_app" {
+resource "aws_instance" "eduflex-app" {
   ami           = "ami-0a2363a9cff180a64"  # Amazon Linux 2 in ap-southeast-2
   instance_type = "t2.micro"
   key_name = "your-ec2-key-name"
