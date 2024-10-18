b599fc1f95afcf504018c4f1aafd054fe0c2f9c0
Siddharth Prabhakar <sid130803@gmail.com>
2024-10-18 12:01:20 +0530

    ami id issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 resource "aws_instance" "eduflex-app" {
-  ami           = "ami-084e237ffb23f8f97"  # Amazon Linux 2 in ap-southeast-2
+  ami           = "ami-0e0e417dfa2028266"  # Amazon Linux 2 in ap-southeast-2
   instance_type = "t2.micro"
   key_name      = var.key_name             # Variable for the SSH key
   
